6e88b52c8d0dacf0ea56954a4ae97eea8bd89029
Bertrand <sitcocolita@gmail.com>
2011-12-02 19:11:26 +0100

    Mise en commentaire de la ligne de security manager, afin de lever l'erreur d'avant

--- src/main.java
+++ src/main.java
@@ -12,12 +12,9 @@ public class main {
 			java.rmi.registry.LocateRegistry.createRegistry(1099);
 			
 			System.out.println("Mise en place du Security Manager ...");
-			System.setSecurityManager(new java.rmi.RMISecurityManager());
-			
+			//System.setSecurityManager(new java.rmi.RMISecurityManager());
 			TestRMIServer testRMIServer = new TestRMIServer();
-			
 			System.out.println("Enregistrement du serveur");
-			
 			Naming.rebind("rmi://"+java.net.InetAddress.getLocalHost()+"/TestRMI",testRMIServer);
 			 
 			//Naming.rebind("rmi://localhost/TestRMI", testRMIServer);
