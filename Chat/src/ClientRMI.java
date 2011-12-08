32fd878640d5d018be7e986c4f30a21342f09308
Guillaume B <guillaumebugnet@yahoo.fr>
2011-12-08 17:27:17 +0100

    test

---
+++ Chat/src/ClientRMI.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.rmi.*;
+
+
+public class ClientRMI {
+
+public static void main(String args[]) {
+	while (true){
+		try {
+			// Récupération d'un stub sur l'objet serveur.
+			Information obj = (Information) Naming.lookup("//Sitcocolita-HP:70/mon_serveur");
+			// Appel d'une méthode sur l'objet distant.
+			BufferedReader in=null;
+			in = new BufferedReader(new InputStreamReader(System.in));
+			String msg=in.readLine().trim();
+			System.out.println(obj.sayHello(msg));
+			} catch (Exception exc) {
+				}
+			}
+	}
+
+}
